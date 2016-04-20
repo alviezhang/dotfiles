4db5ca111820e2837c9aa85b7f724c7b1ed8ee78
alvie <alvie.zhang@gmail.com>
2016-04-20 23:17:57 +0800

    remove useless PATH

--- alvie.zsh
+++ alvie.zsh
@@ -2,7 +2,7 @@ ZSH_THEME="bira"
 
 # export VIRTUAL_ENV_DISABLE_PROMPT=True
 # source ~/.virtualenv/python2/bin/activate
-export PATH=~/bin:$PATH:/usr/local/opt/go/libexec/bin
+export PATH=~/bin:$PATH
 
 UNAME=`uname`
 
@@ -15,6 +15,8 @@ fi
 export GOPATH=~/code/go
 mkdir -p $GOPATH
 
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+
 function proxy() {
     export http_proxy=http://localhost:8118;export https_proxy=http://localhost:8118
 }
@@ -24,4 +26,3 @@ function direct() {
     unset https_proxy;
 }
 
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
