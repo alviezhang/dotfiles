80935d2f6684649b939b5ff5c87a3e5a9309501f
alvie <alvie.zhang@gmail.com>
2017-11-02 15:59:34 +0800

    use pyenv-virtualenv

--- custom.zsh
+++ custom.zsh
@@ -9,16 +9,15 @@ elif [ "$UNAME" = "Darwin" ]; then
     export LANG=en_US.UTF-8
 fi
 
+export PATH="/usr/local/sbin:$PATH"
+
 # Go Settings
 export GOPATH=~/code/go
 mkdir -p $GOPATH
-
+export PATH=$PATH:$GOPATH/bin
 # End
 
-export PATH=$PATH:$GOPATH/bin
-
 # Proxy Settings
-
 function proxy() {
     export http_proxy=http://localhost:8118;export https_proxy=http://localhost:8118
 }
@@ -27,21 +26,10 @@ function direct() {
     unset http_proxy;
     unset https_proxy;
 }
-
 # End
 
 # Python Settings
-
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-
-# virtualenv wrapper
-
-VIRTUALENVWRAPPER_BIN=virtualenvwrapper.sh
-export WORKON_HOME=$HOME/.virtualenvs
-mkdir -p $WORKON_HOME
-
-if which $VIRTUALENVWRAPPER_BIN > /dev/null; then
-    source $VIRTUALENVWRAPPER_BIN
-fi
+if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
 
 # End
