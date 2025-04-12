c676d1a5b8032560e2f36d2999ef9da18fb23d2e
Alvie Zhang <alvie.zhang@gmail.com>
2025-04-12 16:06:53 +0800

    Support customize proxyhost

--- ohmyzsh/dev.zsh
+++ ohmyzsh/dev.zsh
@@ -3,10 +3,12 @@
 # NVM
 [[ -s "/opt/local/share/nvm/init-nvm.sh" ]] && source /opt/local/share/nvm/init-nvm.sh
 
+
 # Go
 export GOPATH="$HOME/.go"
 mkdir -p "$GOPATH"
 path_prepend "$GOPATH/bin"
+
 
 # pyenv
 if [ -s "$HOME/.pyenv/bin/pyenv" ]; then
@@ -16,17 +18,29 @@ if [ -s "$HOME/.pyenv/bin/pyenv" ]; then
   eval "$(pyenv init -)"
 fi
 
+
 # Rust
+export RUSTUP_DIST_SERVER=https://mirrors.ustc.edu.cn/rust-static
+export RUSTUP_UPDATE_ROOT=https://mirrors.ustc.edu.cn/rust-static/rustup
 [[ -s "$HOME/.cargo/env" ]] && source "$HOME/.cargo/env"
 
+
 # 代理配置函数
+# 检查是否存在 ~/.env 文件，如果存在则读取其中的环境变量
+if [[ -f "$HOME/.config/proxy.sh" ]]; then
+  . "$HOME/.config/proxy.sh"
+fi
+
+
 proxy() {
-  export PROXYHOST=10.0.10.20
+  export PROXYHOST=${PROXYHOST:-10.0.10.20}
   export http_proxy=http://$PROXYHOST:7890
   export https_proxy=http://$PROXYHOST:7890
   export all_proxy=socks5://$PROXYHOST:7891
+  echo "✅ Proxy ON"
 }
 
 direct() {
   unset http_proxy https_proxy all_proxy
+  echo "🚫 Proxy OFF"
 }
